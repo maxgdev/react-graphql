5ae07483ffee3f5951946aeda50ec6a7b59a524e
Max <25491453+maxgdev@users.noreply.github.com>
2023-01-08 19:13:02 +0000

    Add DisplayRepo component

--- src/App.js
+++ src/App.js
@@ -3,29 +3,36 @@ import NavButtons from "./NavButtons";
 import SearchBox from "./SearchBox";
 
 function App() {
-  const [userName, setUserName]= useState("Max");
-  const [repoList, setRepoList]= useState(null);
-  const [pageCount, setPageCOunt]= useState(10);
-  const [queryString, setQueryString]= useState("");
-  const [totalCount, setTotalCount]= useState(5);
+  const [userName, setUserName] = useState("Max");
+  const [repoList, setRepoList] = useState(null);
+  const [pageCount, setPageCOunt] = useState(10);
+  const [queryString, setQueryString] = useState("");
+  const [totalCount, setTotalCount] = useState(5);
 
+  const DisplayRepo = () => {
+    return (
+      <ul className="list-group list-group-flush">
+        <p>repo list...1</p>
+        <p>repo list...2</p>
+        <p>repo pst...3</p>
+        <p>repo pst...4</p>
+      </ul>
+    )
+  }
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
       <p>Hey there {userName}</p>
-      <SearchBox 
+      <SearchBox
         totalCount={totalCount}
         pageCount={pageCount}
         queryString={queryString}
-        onTotalchange={(myNumber)=>{setPageCOunt(myNumber)}}
-        onQueryChange={(myString)=>{setQueryString(myString)}}
+        onTotalchange={(myNumber) => { setPageCOunt(myNumber) }}
+        onQueryChange={(myString) => { setQueryString(myString) }}
       />
-      <p>repo list...</p>
-      <p>repo list...</p>
-      <p>repo list...</p>
-      <p>repo list...</p>
+      <DisplayRepo />
       <NavButtons />
     </div>
   );
