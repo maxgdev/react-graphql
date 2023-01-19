e7445684de3e0c939682ae51959fedee08578744
Max <25491453+maxgdev@users.noreply.github.com>
2023-01-19 12:01:46 +0000

    Final styling of RepoInfo

--- src/RepoInfo.js
+++ src/RepoInfo.js
@@ -1,27 +1,25 @@
 const RepoInfo = ({ repo }) => {
 
     return (
-        <li className="list-group-item" key={repo.id.toString()}> 
+        <li className="list-group-item" key={repo.id.toString()}>
             <div className="d-flex justify-content-between align-items-center">
                 <div className="d-flex flex-column">
                     <a className="h5 mb-0 text-decoration-none" href={repo.url} target="_blank">
                         {repo.name}
                     </a>
                     <p className="small">{repo.description}</p>
-
-                    <p>Repository ID: {JSON.stringify(repo.id.toString())}</p>
                 </div>
-                    <span
-                        className={
-                            "px-1 py-0 ms-1 d-inline-block btn btn-sm " +
-                            (repo.viewerSubscription === "SUBSCRIBED"
-                                ? "btn-success"
-                                : "btn-outline-secondary")
-                        }
-                        style={{ fontSize: ".6em" }}>
-                        {repo.viewerSubscription}
-                    </span>
-                </div>
+                <span
+                    className={
+                        "px-1 py-0 ms-1 d-inline-block btn btn-sm " +
+                        (repo.viewerSubscription === "SUBSCRIBED"
+                            ? "btn-success"
+                            : "btn-outline-secondary")
+                    }
+                    style={{ fontSize: ".6em" }}>
+                    {repo.viewerSubscription}
+                </span>
+            </div>
         </li>
     );
 }
